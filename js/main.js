4159c850c0f4565276c3c8e905004b93c87e7833
DeFUCC <fond@fondfrukt.ru>
2020-10-07 15:47:49 +0300

    better

--- js/main.js
+++ js/main.js
@@ -6,11 +6,15 @@ function hydrate() {
   const pic = SVG('#circle-of-tones').css({
     'max-height': '100vh',
   })
-  const circles = pic.find('#circles').children().css({ cursor: 'pointer' })
+  const circles = pic
+    .find('#circles')
+    .children()
+    .css({ cursor: 'pointer', filter: 'grayscale(50%)' })
 
   const letters = pic
     .find('#letters')
     .children()
+    .opacity(0.1)
     .css({ 'pointer-events': 'none' })
 
   circles.on('mouseenter touchstart', activate)
@@ -25,21 +29,19 @@ function hydrate() {
       let spring = new SVG.Spring(500)
       // Listen for a 'note on' message on all channels
       input.addListener('noteon', 'all', function (e) {
-        if (!active) {
-          control()
-          active = true
-          function control() {
-            circles.opacity(0.4)
-          }
-        }
         let note = ((e.note.number + 3) % 12) + 1
-        let circle = pic.findOne('#c' + note)
-        circle.front().transform({ scale: 1.6 }).opacity(1)
+        let circle = pic.findOne('#o' + note)
+        let letter = pic.findOne('#l' + note)
+        letter.opacity(1)
+
+        circle.transform({ scale: 6 }).opacity(1)
       })
 
       input.addListener('noteoff', 'all', function (e) {
         let note = ((e.note.number + 3) % 12) + 1
-        let circle = pic.findOne('#c' + note)
+        let circle = pic.findOne('#o' + note)
+        let letter = pic.findOne('#l' + note)
+        letter.opacity(0.1)
         circle.transform({ scale: 1 }).opacity(0.6)
       })
     }
@@ -48,12 +50,12 @@ function hydrate() {
   function activate(e) {
     const el = SVG(e.target)
     circles.opacity(0.25)
-    el.front().opacity(1).animate(200).transform({ scale: 1.6 })
+    el.front().opacity(1).transform({ scale: 1.6 })
   }
 
   function deactivate(e) {
     const el = SVG(e.target)
     circles.animate(100).opacity(1)
-    el.animate(200).transform({ scale: 1 })
+    el.transform({ scale: 1 })
   }
 }
